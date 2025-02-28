3a77f1355821f88ed8d254e9d45d457bf64699ee
Taylor Nodell <taylor.nodell@techin.site>
2025-03-01 10:39:51 +1100

    Removes unneccessary call for ipcBridge.sendMessage('folder-selection') and log

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -6,14 +6,3 @@ if (container) {
   const root = createRoot(container);
   root.render(<App />);
 }
-
-// Listen for messages from the main process
-if (window.electron) {
-  window.electron.ipcBridge.sendMessage('folder-selection', ['change-folder']);
-
-  window.electron.ipcBridge.on('processed-images', (images) => {
-    console.log('Received images:', images);
-  });
-} else {
-  console.error('window.electron is undefined. Check your preload script.');
-}
